39fc71bfeb118bd9d7ae2a6caf72de6dd796271e
jkliujun <jkliujun@gmail.com>
2025-06-27 12:45:40 +0800

    Change base image

--- order-service/src/main/docker/main.sh
+++ order-service/src/main/docker/main.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #mkdir /home/tl/.ssh
-exec gosu tl:tlgroup java -server ${JAVA_GC} -Xms512m -Xmx512m ${JAVA_OPTS} \
+#exec gosu tl:tlgroup java -server ${JAVA_GC} -Xms512m -Xmx512m ${JAVA_OPTS} \
+exec java -server ${JAVA_GC} -Xms512m -Xmx512m ${JAVA_OPTS} \
   -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:28964 \
     -cp /app/resources:/app/classes:/app/libs/* \
 	${MAIN_CLASS}
