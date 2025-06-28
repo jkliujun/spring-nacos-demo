ab3bc43ae3c0c06be4c59406828190558c8fec80
jkliujun <jkliujun@gmail.com>
2025-06-28 10:35:06 +0800

    Fix grpc core dependency

--- user-service/src/test/java/com/example/userservice/UserServiceGrpcImplTest.java
+++ user-service/src/test/java/com/example/userservice/UserServiceGrpcImplTest.java
@@ -1,15 +1,19 @@
 package com.example.userservice;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import com.example.grpcproto.UserRequest;
 import com.example.grpcproto.UserResponse;
 import com.example.grpcproto.UserServiceGrpc;
-import io.grpc.inprocess.InProcessChannelBuilder;
-import io.grpc.inprocess.InProcessServerBuilder;
+
 import io.grpc.ManagedChannel;
 import io.grpc.Server;
-import org.junit.jupiter.api.*;
-
-import static org.junit.jupiter.api.Assertions.*;
+import io.grpc.inprocess.InProcessChannelBuilder;
+import io.grpc.inprocess.InProcessServerBuilder;
 
 class UserServiceGrpcImplTest {
 
@@ -39,6 +43,10 @@ class UserServiceGrpcImplTest {
         UserServiceGrpc.UserServiceBlockingStub stub = UserServiceGrpc.newBlockingStub(channel);
         UserRequest request = UserRequest.newBuilder().setUserId("123").build();
         UserResponse response = stub.getUserById(request);
+        System.out.println("------------------------------------------------------------------------");
+
+        System.out.println("Response: " + response.getResponseMessage());
+        System.out.println("------------------------------------------------------------------------");
         assertEquals("User ID: 123", response.getResponseMessage());
     }
 }
